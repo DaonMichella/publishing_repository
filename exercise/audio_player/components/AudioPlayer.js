d4bedaf5c049349be58788dfa5ac1e325078a7da
jeonsooyoung <peace2332@naver.com>
2021-05-13 23:11:34 +0900

    board task add

---
+++ exercise/audio_player/components/AudioPlayer.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const AudioPlayer = () => {
+  return (
+    <div>
+      Audio Player
+    </div>
+  )
+}
+
+export {AudioPlayer}//curly brackets
+/* 모듈은 export default라는 특별한 문법을 지원합니다. export default를 사용하면 
+'해당 모듈엔 개체가 하나만 있다’는 사실을 명확히 나타낼 수 있습니다 */
+
