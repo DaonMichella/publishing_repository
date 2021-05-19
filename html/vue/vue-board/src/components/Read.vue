96dea4d4f5e5579fdeaa07971d14fd1143d1f0e8
jeonsooyoung <peace2332@naver.com>
2021-05-19 23:36:29 +0900

    add router

--- html/vue/vue-board/src/components/Read.vue
+++ html/vue/vue-board/src/components/Read.vue
@@ -85,7 +85,7 @@
 
 <script>
 export default {
-  name: 'HelloWorld',
+  name: 'Read',
   data () {
     return {
       msg: 'Welcome to Your Vue.js App'
@@ -93,21 +93,3 @@ export default {
   }
 }
 </script>
-
-<!-- Add "scoped" attribute to limit CSS to this component only -->
-<style scoped>
-h1, h2 {
-  font-weight: normal;
-}
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-a {
-  color: #42b983;
-}
-</style>
---
+++ html/vue/vue-board/src/data/index.js
@@ -0,0 +1,17 @@
+export default [
+  {
+    writer :'아이린',
+    title :'레드벨벳',
+    content :'1'
+  },
+  {
+    writer :'예리',
+    title :'레드벨벳',
+    content :'2'
+  },
+  {
+    writer :'웬디',
+    title :'레드벨벳',
+    content :'3'
+  }
+]
