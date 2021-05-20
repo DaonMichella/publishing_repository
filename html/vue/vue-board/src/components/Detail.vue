3db648385a7a5dc9fc9b8f2ac01c31ba4831a042
jeonsooyoung <peace2332@naver.com>
2021-05-20 10:19:41 +0900

    board example

---
+++ html/vue/vue-board/src/components/Detail.vue
@@ -0,0 +1,44 @@
+<template>
+  <div class="wrap">
+    <ul>
+      <li>{{data.writer}}</li>
+      <li>{{data.title}}</li>
+      <li>{{data.content}}</li>
+    </ul>
+    <button type="button" @click="updateData">수정</button>
+    <button type="button" @click="deleteData">삭제</button>
+  </div>
+</template>
+
+<script>
+import data from '@/data';
+export default {
+  name: 'Detail',
+  data () {
+    const index = this.$route.params.contentId
+    return {
+      data : data[index],
+      index : index
+    }
+  },
+  methods : {
+    updateData () {
+      this.$router.push ({
+        name : 'Create',
+        params : {
+          contentId : this.index
+        }
+      })
+    },
+    deleteData () {
+      data.splice(this.index,1)
+      this.$router.push({
+        path : "/"
+      })
+    }
+  }
+}
+</script>
+<style>
+  ul,li {list-style:none;}
+</style>
--- html/vue/vue-board/src/router/index.js
+++ html/vue/vue-board/src/router/index.js
@@ -1,6 +1,8 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import Read from '@/components/Read'
+import Create from '@/components/Create'
+import Detail from '@/components/Detail'
 
 Vue.use(Router)
 
@@ -10,6 +12,17 @@ export default new Router({
       path: '/',
       name: 'Read',
       component: Read
-    }
+    },
+    {
+      path: '/create/:contentId?',
+      name: 'Create',
+      component: Create
+    },
+    {
+      path: '/detail/:contentId',
+      name: 'Detail',
+      component: Detail
+    },
+    
   ]
 })
