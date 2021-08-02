af26b13b857dbf56bda427d471b963d075f84e96
jeonsooyoung <thepanda2332@gmail.com>
2021-08-02 22:23:11 +0900

    node board update

--- exercise/notice_generator/lib/template.js
+++ exercise/notice_generator/lib/template.js
@@ -1,31 +1,55 @@
 module.exports = {
-    structure: function (title, body, control){
+    structure: function (title,list, body, control){
       return `
-      <!doctype html>
-      <html>
-        <head>
-          <meta charset="utf-8">
+      <!DOCTYPE html>
+      <html lang="ko">
+      <head>
+          <meta charset="UTF-8">
+          <meta http-equiv="X-UA-Compatible" content="IE=edge">
+          <meta name="viewport" content="width=device-width, initial-scale=1.0">
           <title>${title}</title>
           <link type="text/css" rel="stylesheet" href="style.css"/>
-        </head>
-        <body>
+
+      </head>
+      <body>
           <div id="wrap">
-            <h1 class="main-title">게시글 생성<span class="sub">제목/내용 저장하기</span></h1>
-            ${body}            
-            ${control}
-          </div>            
-        </body>
+            <h1 class="main-title">${title}</h1>
+
+              <div class="tbl-type01">
+                <table>
+                    <caption>${title}테이블</caption>
+                    <thead>
+                        <tr>
+                            <th scope="row" colspan="3">${title}</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        ${list}
+                    </tbody>
+
+                </table>
+              </div>
+              ${body}    
+              ${control}
+              
+
+          </div>
+      </body>
       </html>
-        `;
+        `
     },
-    // list : function (filelist) {
-    //   var list = '<ul>';
-    //   for (var list_i = 0; list_i < filelist.length; list_i++) {    
-    //     list += `<li><a href="/?id=${filelist[list_i]}">${filelist[list_i]}</a></li>`;
-    //   }
-    //   list += '</ul>';
-    //   return list;
-    // }
+    list : function (filelist) {
+      var list = '<tbody>';
+      for (var list_i = 0; list_i < filelist.length; list_i++) {    
+        list += `
+        <tr>
+            <td>${list_i}</td>
+            <td><a v-bind:href='data/${filelist[list_i]}' target="_blank">${filelist[list_i]}</a></td>
+        </tr>`
+      }
+      list += '</tbody>';
+      return list;
+    }
   
     
   }
--- exercise/notice_generator/main.js
+++ exercise/notice_generator/main.js
@@ -4,141 +4,162 @@ var url = require('url');
 var qs = require('querystring');
 var template = require('./lib/template.js')
 var path = require('path')
-var style = require('./style.css')
+
+var mimeType = {//확장자에 따라서 content-type header 값을 동적으로 생성
+  //html  단일 페이지 뿐만 아니라 모든 정적요소 불러오기 위함
+  ".ico": "image/x-icon",
+  ".html": "text/html",
+  ".txt ": "text/html",
+  ".js": "text/javascript",
+  ".css": "text/css",
+  ".png": "image/png",
+  ".jpg": "image/jpeg"
+}
 
 var app = http.createServer(function(request,response){
   var _url = request.url;
-  var queryData = url.parse(_url, true).query;
-  var pathname = url.parse(_url, true).pathname;
-  if(pathname === '/'){
-    if(queryData.id === undefined){
-      fs.readdir('./data', function(error, filelist){
-        var title = '기사제목';
-        var description = '기사내용';
-        var list = template.list(filelist);
-        var html = template.structure(title, list, `<h2>${title}</h2><p>${description}</p>`,`<button type='button' class="btn blue">공지사항 등록</button>`);
-        response.writeHead(200);
-        response.end(html);
-      });
-    } else {
-        fs.readdir('./data', function(error, filelist) {
-          var filteredId = path.parse(queryData.id).base
-
-          fs.readFile(`data/${filteredId}`, 'utf8', function(err, description){
-              var title = queryData.id;
-              var list = template.list(filelist);
-              var html = template.structure(title, list, `
-              <h2>${title}</h2>${description}`,
-              `
-                <button type='button'>공지사항등록</button>
-                <a href="/create">create</a> 
-                <a href="/update?id=${title}">update</a> 
-                <form action="delete_process" method="post" onsubmit="">
-                  <input type="hidden" name="id" value="${title}">
-                  <input type="submit" value="delete">
-                </form>
-
-              `);
-              response.writeHead(200);
-              response.end(html);
-          });
-        });     
-    }
-  } else if(pathname === '/create'){
-    fs.readdir('./data', function(error, filelist){
-      var title = '공지사항 등록';
-      var list = template.list(filelist);//이 부분 오타 매서드명 오타 
-      var html = template.structure(title, list, `
-          <form action="/create_process" method="post">
-          <div id="wrap">
-            <h1 class="main-title">게시글 생성</h1>
-            <div class="input-area">
-              <div class="inp-txt">
-                <input type="text" name="title" placeholder="title">
-              </div>
-              <textarea name="description" placeholder="description" title="글 내용"></textarea>        
-            </div>
-            <input type="submit">
-    
-          </div>           
-          </form>
-        `,``);
-      response.writeHead(200);//성공
-      response.end(html);
-    });
-  } else if(pathname === '/create_process'){//Post 방식으로 보낸 데이터를 Node js에서 불러오기!!
-    var body = '';
-    request.on('data',function(data){//
-        body += data;
-    })//node js로 접속 들어올때마다 콜백 함수로 node 호출
-    request.on('end',function(){
-        var post = qs.parse(body);
-        var title = post.title;
-        var description = post.description;
-        fs.writeFile(`data/${title}`, description,'utf8',
-        function(err){
-          response.writeHead(302, {Location:`/?id=${title}`});
-          response.end();
-        })      
+  var ext = path.parse(_url).ext
+  var publicPath = path.join(__dirname, "./public")
+  
+  if(Object.keys(mimeType).includes(ext)) {//mine Type 딕셔너리로 있을 경우
+    fs.readFile(`${publicPath}${_url}`,(err,data)=> {
+      if(err) {
+        response.statusCode = 404;
+        response.end('Not found')
+      } else {
+        response.statusCode = 200
+        response.setHeader("Content-Type", mimeType[ext])//응답을 화면으로 보여줌
+        response.end(data)
+      }
     })
-
-  } else if(pathname === '/update'){//업데이트 했을 때 보여지는 화면
-    fs.readdir('./data', function(error, filelist) {
-      var filteredId = path.parse(queryData.id).base
-      fs.readFile(`data/${filteredId}`, 'utf8', function(err, description){
-          var title = queryData.id;
-          var sanitizedTitle = title;
-          var sanitizedDescription = description;
-          var list = template.list(filelist);
-          //form 입력 후 데이터를 어디로 보낼 것인가
-          var html = template.structure(title, list,          
-          `
-          <form action="/update_process" method="post">
-            <input type="hidden" name="id" value="${title}"><!--업데이트 될 데이터가 쉉될수 있으므로 hidden 에 저장-->
-            <p><input type="text" name="title" placeholder="title" value="${title}"></p>
-            <p>
-              <textarea name="description" placeholder="description">${description}</textarea>
-            </p>
-            <p>
-              <input type="submit">
-            </p>
-          </form>
-        `,`<a href="/create">create</a> <a href="/update?id=${title}">update</a>`
-        );
+    
+  } else {
+    var _url = request.url;
+    var queryData = url.parse(_url, true).query;
+    var pathname = url.parse(_url, true).pathname;
+    if(pathname === '/'){
+      fs.readdir('./data', function(error, filelist){
+        var list = template.list(filelist);
+        var html = template.structure('Main', list, `
+            <form action="/create_process" method="post">
+            <div id="wrap">
+              <h1 class="main-title">게시글 생성</h1>
+              <div class="input-area">
+                <div class="inp-txt">
+                  <input type="text" name="title" placeholder="title">
+                </div>
+                <textarea name="description" placeholder="description" title="글 내용"></textarea>        
+              </div>
+      
+            </div>           
+            </form>
+          `,`
+          <div class="btn-group">
+            <a href="notice_new.html" class="btn blue"><span class="txt">새 공지사항</span></a>
+          </div>
+          `);
           response.writeHead(200);
           response.end(html);
-      });
-    });   
-  } else if(pathname === '/update_process'){
-    var body = '';
-    //request 객체에 이벤트 리스너를 등록해서 다른 스트림에 파이프로 연결 
-    //스트림의 data와 end 이벤트에 이벤트 리스너 등록
-    //스트림 : 스트리밍 데이터로 작업하기 위한 추상적인 인터페이스 
-    //입 출력 기기나 프로세스, 파일 등 어디로 가는지, 어디에서 왔는지 상관없이 통일된 방식으로 데이터를 다루기 위한 가상의 개념 
-    //노드에서 사용하는 많은 오브젝트들이 stream object 임. 
-    //http 서버의 request나 process.stdout 도 스트림 인스턴스
-    // 스트림들은 읽을 수 있거나 쓸 수 있거나, 혹은 둘다 가능 
-    request.on('data',function(data){
-      body += data;
-    })
-    request.on("end",function(){
-      var post = qs.parse(body)      
-      var id = post.id;
-      var title = post.title;
-      var description = post.description;
 
-      fs.rename(`data/${id}`,`data/${title}`,function(error){
-        fs.writeFile(`data/${title}`, description,'utf8',
-        function(err){
-          response.writeHead(302, {Location:`/?id=${title}`});
-          response.end();
-        })     
       })
-    })
-  } else {
-    response.writeHead(404);
-    response.end('Not found');
+    } else if(pathname === '/create'){
+      fs.readdir('./data', function(error, filelist){
+        var title = '공지사항 등록';
+        var list = template.list(filelist);//이 부분 오타 매서드명 오타 
+        var html = template.structure(title, list, `
+            <form action="/create_process" method="post">
+            <div id="wrap">
+              <h1 class="main-title">게시글 생성</h1>
+              <div class="input-area">
+                <div class="inp-txt">
+                  <input type="text" name="title" placeholder="title">
+                </div>
+                <textarea name="description" placeholder="description" title="글 내용"></textarea>        
+              </div>
+              <input type="submit">
+      
+            </div>           
+            </form>
+          `,``);
+        response.writeHead(200);//성공
+        response.end(html);
+      });
+    } else if(pathname === '/create_process'){//Post 방식으로 보낸 데이터를 Node js에서 불러오기!!
+      var body = '';
+      request.on('data',function(data){//
+          body += data;
+      })//node js로 접속 들어올때마다 콜백 함수로 node 호출
+      request.on('end',function(){
+          var post = qs.parse(body);
+          var title = post.title;
+          var description = post.description;
+          fs.writeFile(`data/${title}`, description,'utf8',
+          function(err){
+            response.writeHead(302, {Location:`/?id=${title}`});
+            response.end();
+          })      
+      })
+  
+    } else if(pathname === '/update'){//업데이트 했을 때 보여지는 화면
+      fs.readdir('./data', function(error, filelist) {
+        var filteredId = path.parse(queryData.id).base
+        fs.readFile(`data/${filteredId}`, 'utf8', function(err, description){
+            var title = queryData.id;
+            var sanitizedTitle = title;
+            var sanitizedDescription = description;
+            var list = template.list(filelist);
+            //form 입력 후 데이터를 어디로 보낼 것인가
+            var html = template.structure(title, list,          
+            `
+            <form action="/update_process" method="post">
+              <input type="hidden" name="id" value="${title}"><!--업데이트 될 데이터가 쉉될수 있으므로 hidden 에 저장-->
+              <p><input type="text" name="title" placeholder="title" value="${title}"></p>
+              <p>
+                <textarea name="description" placeholder="description">${description}</textarea>
+              </p>
+              <p>
+                <input type="submit">
+              </p>
+            </form>
+          `,`<a href="/create">create</a> <a href="/update?id=${title}">update</a>`
+          );
+            response.writeHead(200);
+            response.end(html);
+        });
+      });   
+    } else if(pathname === '/update_process'){
+      var body = '';
+      //request 객체에 이벤트 리스너를 등록해서 다른 스트림에 파이프로 연결 
+      //스트림의 data와 end 이벤트에 이벤트 리스너 등록
+      //스트림 : 스트리밍 데이터로 작업하기 위한 추상적인 인터페이스 
+      //입 출력 기기나 프로세스, 파일 등 어디로 가는지, 어디에서 왔는지 상관없이 통일된 방식으로 데이터를 다루기 위한 가상의 개념 
+      //노드에서 사용하는 많은 오브젝트들이 stream object 임. 
+      //http 서버의 request나 process.stdout 도 스트림 인스턴스
+      // 스트림들은 읽을 수 있거나 쓸 수 있거나, 혹은 둘다 가능 
+      request.on('data',function(data){
+        body += data;
+      })
+      request.on("end",function(){
+        var post = qs.parse(body)      
+        var id = post.id;
+        var title = post.title;
+        var description = post.description;
+  
+        fs.rename(`data/${id}`,`data/${title}`,function(error){
+          fs.writeFile(`data/${title}`, description,'utf8',
+          function(err){
+            response.writeHead(302, {Location:`/?id=${title}`});
+            response.end();
+          })     
+        })
+      })
+    } else {
+      response.writeHead(404);
+      response.end('Not found');
+    }
+
   }
+
 
 
 
